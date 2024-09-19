59dbd98c594a241cafa502887b5dba15f3a2dc60
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-09-18 17:26:17 -0700

    Adds nav and footer components

---
+++ src/components/footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div class="card w-100">
+        <div class="card-body">
+            Made with &#x1F49C; by xChrisxWilliamsx
+        </div>
+        </div>        
+    );
+};
+
+export default Footer;
