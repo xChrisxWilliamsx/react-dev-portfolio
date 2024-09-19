57ec983a00d2bebf5189d193da775efe2f8b1664
xChrisxWilliamsx <williamsc0325@gmail.com>
2024-09-19 16:31:52 -0700

    Adds page files

---
+++ src/pages/contactMe.jsx
@@ -0,0 +1,17 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1 className="text-center">Contact me at:</h1>
+            <p className="text-center">
+                <a href="https://www.linkedin.com/in/williamsc1"  target="_blank" title="Click to open LinkedIn in a new tab" alt="linkedin profile">LinkedIn</a>
+                <br />
+                <a href="https://github.com/xChrisxWilliamsx" target="_blank" title="Click to open Github in a new tab" alt="GitHub profile">GitHub</a>
+                <br />
+                Email me at: williamsc0325@gmail.com
+            </p>
+        </div>
+        
+    );
+};
+
+export default Contact;
---
+++ src/pages/resume.jsx
@@ -0,0 +1,60 @@
+const Resume = () => {
+    return (
+        <div>
+            <p className="text-center py-5">
+                <h1>
+                    Chris Williams
+                </h1> 
+                <br />
+                <p>
+                Motivated Sales/Service management professional proficient in solving complex issues while consistently delivering quality service.  Skilled in handling customer account inquiries and quotes, accurately providing information/solutions to ensure resolution of product/service concerns and customer satisfaction.  Recently Completed a full stack coding bootcamp partnered through the University of Oregon.  Ongoing collaboration with senior leadership and peers to create and implement better working practices to consistently achieve customer retention goals and expand customer install base (retail and B2B), developing new accounts, and maximizing ROI.
+                </p>
+                <br />
+                <h2>
+                    Skills:
+                </h2>
+                <br /> 
+                <p>
+                    <li>
+                        HTML
+                    </li>
+                    <li>
+                        CSS
+                    </li>
+                    <li>
+                        JavaScript
+                    </li>
+                    <li>
+                        SQL
+                    </li>
+                    <li>
+                        MongoDB
+                    </li>
+                    <li>
+                        B2B Sales
+                    </li>
+                    <li>
+                        Management
+                    </li>
+                </p>
+                <br />
+                <h3>
+                    Work Experience:
+                </h3>
+                <br />
+                <p>
+                    Automotive Service Advisor - August 2017 - March 2024
+                </p>
+                <h3>
+                    Education:
+                </h3>
+                <br />
+                <p>
+                    Full Stack Coding Bootcamp 2U x University of Oregon - April 2024 - September 2024
+                </p>
+            </p>
+        </div>
+    );
+};
+
+export default Resume;
